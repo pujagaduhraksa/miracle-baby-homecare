698c9ff9ed1228032a40ee1edfeaf442382874fe
puja <pujaraksa.id@gmail.com>
2023-02-20 20:25:11 +0800

    Nav and Hero Section

---
+++ main.js
@@ -0,0 +1,10 @@
+// Set Toogle
+
+$(document).ready(function(){
+    $('.hamburger-menu').click(function(){
+        $('.hamburger-menu').toggleClass('active')
+        $('nav').toggleClass('active')
+    })
+})
+
+// End Set Toogle
